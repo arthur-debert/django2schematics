72167d7e8072e96e57885d5bc01909bd97e04266
Arthur Debert <arthur@stimuli.com.br>
2014-08-16 19:58:54 -0300

    Implemented point field

--- django2schematics/djtypes.py
+++ django2schematics/djtypes.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 import random
-from schematics.types import BooleanType
+from schematics.types import BooleanType, BaseType
 
 
 class NullBooleanType(BooleanType):
@@ -11,3 +11,16 @@ class NullBooleanType(BooleanType):
         if value is None:
             return None
         return super(NullBooleanType, self).to_native(value, context)
+
+
+class PointType(BaseType):
+    def __init__(self, srid, x, y):
+        super(PointType, self).__init__()
+        try:
+            from django.contrib.gis.geos import Point
+        except ImportError:
+            raise Exception("No geojango support deceted")
+        self.value = Point(x=x, y=y, srid=srid)
+
+    def to_native(self, value, context=None):
+        return value.coords if value else None
