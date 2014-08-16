10ec9acdff67c63b3a1b6b3a47635cf034069c40
Arthur Debert <arthur@stimuli.com.br>
2014-08-16 18:39:14 -0300

    Small pepping

--- django2schematics/djtypes.py
+++ django2schematics/djtypes.py
@@ -2,8 +2,8 @@ from __future__ import absolute_import
 import random
 from schematics.types import BooleanType
 
-class NullBooleanType(BooleanType):
 
+class NullBooleanType(BooleanType):
     def _mock(self, context=None):
         return random.choice([None, True, False])
 
