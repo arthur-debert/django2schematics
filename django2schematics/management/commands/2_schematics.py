788994cff90166d36d118a9855c920b6cccfeb40
Arthur Debert <arthur@stimuli.com.br>
2014-08-16 22:21:29 -0300

    Fixed django command name

--- django2schematics/management/commands/2_schematics.py
+++ django2schematics/management/commands/2_schematics.py
@@ -1,14 +1,14 @@
-from django.core.management.base import LabelCommand
+from django.core.management.base import BaseCommand
 from django2schematics.command_helpers import get_output
 
 
-class SchematicsOutputCommand(LabelCommand):
+class Command(BaseCommand):
     args = '<app or model app_name or model ...>'
     label = 'label'
 
     def handle(self, *labels, **options):
         return get_output(
+            labels,
             to_file=options.get('to_file', False),
             auto_file_name=options.get('auto_file_name', None),
-            *labels
         )
